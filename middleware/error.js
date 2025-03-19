475e6b051ddbfc4162f8700ea383312a8c5d5243
PBA443 <donbhagy@gmail.com>
2025-03-19 15:44:02 +0530

    eroor handler refactor

--- middleware/error.js
+++ middleware/error.js
@@ -1,5 +1,5 @@
 //error handling in application
 module.exports = function (err, req, res, next) {
-  console.error("Error in route handler:", error.message);
-  res.status(500).json({ message: "Internal Server Error" }); // Send a 500 error response
+  console.error("Error in route handler:");
+  res.status(500).send("Internal Server Error"); // Send a 500 error response
 };
